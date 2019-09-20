d9f57c081de1cbfc9d4c6a8041d987409db51ff3
jcheron <myaddressmail@gmail.com>
2019-09-21 00:22:02 +0200

    initial commit

---
+++ src/Ubiquity/servers/workerman/WorkermanServer.php
@@ -0,0 +1,126 @@
+<?php
+namespace Ubiquity\servers\workerman;
+
+use Workerman\Protocols\Http;
+use Workerman\Protocols\HttpCache;
+use Workerman\Worker;
+use Ubiquity\utils\http\foundation\WorkermanHttp;
+
+class WorkermanServer {
+
+	/**
+	 * @var \Workerman\Worker
+	 */
+	private $server;
+
+	private $httpInstance;
+
+	private $config;
+
+	private $basedir;
+
+	private $options;
+	
+	private $events=[];
+
+	/**
+	 *
+	 * @return int
+	 */
+	private function getPid(): int {
+		$file = $this->getPidFile();
+		if (! \file_exists($file)) {
+			return 0;
+		}
+		$pid = (int) \file_get_contents($file);
+		if (! $pid) {
+			$this->removePidFile();
+			return 0;
+		}
+		return $pid;
+	}
+
+	/**
+	 * Get pid file.
+	 *
+	 * @return string
+	 */
+	private function getPidFile(): string {
+		return Worker::$pidfile;
+	}
+
+	/**
+	 * Remove the pid file.
+	 */
+	private function removePidFile(): void {
+		$file = $this->getPidFile();
+		if (\file_exists($file)) {
+			\unlink($file);
+		}
+	}
+
+	/**
+	 * Configure the created server.
+	 */
+	private function configure($http) {
+		$http->set($this->options);
+	}
+	
+	private function addEvents($http){
+		foreach ($this->events as $event=>$callback) {
+			$http->on($event,$callback);
+		}
+	}
+
+	public function init($config, $basedir) {
+		$this->config = $config;
+		$this->basedir = $basedir;
+		$this->httpInstance=new WorkermanHttp();
+		\Ubiquity\controllers\Startup::init($config);
+	}
+
+	/**
+	 * Get swoole configuration option value.
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function getOption(string $key) {
+		$option = $this->options[$key];
+		if (! $option) {
+			throw new \InvalidArgumentException(sprintf('Parameter not found: %s', $key));
+		}
+		return $option;
+	}
+
+	public function setOptions($options = []) {
+
+	}
+
+	public function run($host, $port, $options = null) {
+		$this->server=new Worker("http://$host:$port",$options);
+		$this->server->onMessage(static function($connection,$datas){
+			$this->handle($connection,$datas);
+		});
+	}
+	
+
+	protected function handle(ConnectionInterface $connection,$datas) {
+		$request->get['c'] = '';
+		$uri = \ltrim(\urldecode(\parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/');
+		if ($uri == null || ! \file_exists($this->basedir . '/../' . $uri)) {
+			$request->get['c'] = $uri;
+		} else {
+			Http::header('Content-Type', HttpCache::$header['accept'] ?? 'text/html; charset=utf-8');
+			$connection->send(\file_get_contents($this->basedir . '/../' . $uri));
+			return;
+		}
+
+		$this->httpInstance->setDatas($datas);
+		\ob_start();
+		\Ubiquity\controllers\Startup::setHttpInstance($this->httpInstance);
+		\Ubiquity\controllers\Startup::forward($request->get['c']);
+		$connection->send(\ob_get_clean());
+	}
+}
+
---
+++ src/Ubiquity/utils/http/foundation/WorkermanHttp.php
@@ -0,0 +1,66 @@
+<?php
+namespace Ubiquity\utils\http\foundation;
+
+use Workerman\Protocols\Http;
+
+/**
+ * Http instance for Swoole.
+ * Ubiquity\utils\http\foundation$WorkermanHttp
+ * This class is part of Ubiquity
+ *
+ * @author jcheron <myaddressmail@gmail.com>
+ * @version 1.0.0
+ *         
+ */
+class WorkermanHttp extends AbstractHttp {
+
+	private $headers = [];
+
+	private $responseCode = 200;
+	
+	private $datas;
+
+
+	public function getAllHeaders() {
+		return $this->headers;
+	}
+	
+	public function setDatas($datas){
+		return $this->datas;
+	}
+
+	public function header($key, $value, bool $replace = false, int $http_response_code = null) {
+		$this->headers[$key] = $value;
+		if ($http_response_code != null) {
+			$this->responseCode = $http_response_code;
+		}
+		Http::header($key.' :'.$value,$replace,$http_response_code);
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getResponseCode() {
+		return $this->responseCode;
+	}
+
+	/**
+	 *
+	 * @param int $responseCode
+	 */
+	public function setResponseCode($responseCode) {
+		if ($responseCode != null) {
+			$this->responseCode = $responseCode;
+		}
+	}
+
+	public function headersSent(string &$file = null, int &$line = null) {
+		return \headers_sent($file, $line);
+	}
+
+	public function getInput() {
+		return $this->datas;
+	}
+}
+
